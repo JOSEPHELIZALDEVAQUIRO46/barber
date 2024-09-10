d3c3c30b8779013af6f30d876c87a3013161ff96
JOSEPHELIZALDEVAQUIRO46 <132629607+JOSEPHELIZALDEVAQUIRO46@users.noreply.github.com>
2024-09-10 16:19:06 -0500

    Update settings.py

--- barber/barber/settings.py
+++ barber/barber/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME':'barber',
         'USER':'root',
-        'PASSWORD':'Joseph(462005)',
+        'PASSWORD':'',
         'HOST':'localhost',
         'PORT':'3306',
     }
