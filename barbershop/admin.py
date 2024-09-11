a0a7a73d6f17f42525765ba38b251eed75be4fec
Joseph Elizalde Váquiro <joseph_elizalde@soy.sena.edu.co>
2024-09-11 16:57:15 -0500

    AWS

--- barbershop/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Barberia, Barbero, Servicio, Contabilidad, Promociones
-
-admin.site.register(Barberia)
-admin.site.register(Barbero)
-admin.site.register(Servicio)
-admin.site.register(Contabilidad)
-admin.site.register(Promociones)
