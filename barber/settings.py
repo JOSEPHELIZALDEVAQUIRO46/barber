fd0a01fee71263fd2e5fcc694621ba93c8bbc89a
Joseph Elizalde Váquiro <joseph_elizalde@soy.sena.edu.co>
2024-09-17 16:00:49 -0500

    AWS

--- barber/settings.py
+++ barber/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-n+@^8-e@_0hybw-$hvtb7^ck1%@ru-*)a5q-s2fagr-i_fc3gn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.144.229.60', 'localhost']
 
 
 # Application definition
@@ -83,8 +83,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME':'barber',
         'USER':'root',
-        'PASSWORD':'Joseph(462005)',
-        'HOST':'localhost',
+        'PASSWORD':'Duberney(123)',
+        'HOST':'database.cvu6so208y2g.us-east-2.rds.amazonaws.com',
         'PORT':'3306',
     }
 }
