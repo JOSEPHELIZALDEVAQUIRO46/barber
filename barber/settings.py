b64d7389d016f7d96a3a13d17fcfb0bb37b98d1c
Joseph Elizalde Váquiro <joseph_elizalde@soy.sena.edu.co>
2024-10-01 09:12:13 -0500

    AWS

--- barber/settings.py
+++ barber/settings.py
@@ -130,12 +130,11 @@ USE_TZ = True
 STATIC_URL = '/staticfiles/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'barber/staticfiles'),  
+    os.path.join(BASE_DIR, 'barber/staticfiles'),  #
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'barber/staticfiles')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
