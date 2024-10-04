8cc66e43f362949bedb94586964ebad3c5fff3fc
Joseph Elizalde Váquiro <joseph_elizalde@soy.sena.edu.co>
2024-10-04 16:58:12 -0500

    AWS

--- barber/settings.py
+++ barber/settings.py
@@ -135,7 +135,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'barbershop/static'),  
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'barber/staticfiles')
+SSTATIC_ROOT = '/barber/barber/staticfiles/'
+
 
 
 
